72af502718a0a1624a8593268f808fb1b46b26d0
manuelguijarro <manu2506.1994@gmail.com>
2024-01-09 00:39:51 +0100

    add_cors

--- main.py
+++ main.py
@@ -2,11 +2,18 @@
 from time import time  
 from fastapi import FastAPI,__version__
 from routers.users_db import router as user_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI() # This is what will be refrenced in config
 
 app.include_router(user_router)
-
+# Añade el middleware CORS
+app.add_middleware(
+    CORSMiddleware(
+        allow_origins=["https://backend-lotr-api-vercel-server.vercel.app/"],
+        allow_methods=["GET", "POST", "PUT", "DELETE"],
+        allow_headers=["Content-Type", "Authorization"],
+    )
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
