49658c2ed54b66c8f835e1e3cb61b41465adf100
manuelguijarro <manu2506.1994@gmail.com>
2024-01-08 19:45:47 +0100

    deploy_vercel

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-# main.py  
-from fastapi import FastAPI  
+# main.py
+from time import time  
+from fastapi import FastAPI,__version__
 
 app = FastAPI() # This is what will be refrenced in config
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get('/ping')
+async def hello():
+    return {'res': 'pong', 'version': __version__, "time": time()}
