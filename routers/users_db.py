1ca3171b2b0b33dad77edcd08eb3211b18b46dc6
manuelguijarro <manu2506.1994@gmail.com>
2024-01-08 21:06:11 +0100

    add_router_users

--- routers/users_db.py
+++ routers/users_db.py
@@ -9,3 +9,20 @@ router = APIRouter(
 @router.get("/")
 async def get_users():
   return {"message": "Hello from users db"}
+
+@router.get("/{id}")
+async def get_user(id: str):
+  return {"message": "Hello from users db", "id": id}
+
+
+@router.post("/")
+async def create_user():
+  return {"message": "Hello from create users db"}
+
+@router.put("/{username}")
+async def update_user(username: str):
+  return {"message": "Hello from update users db", "username": username}
+
+@router.delete("/{id}")
+async def delete_user(id: str):
+  return {"message": "Hello from delete users db", "id": id}
