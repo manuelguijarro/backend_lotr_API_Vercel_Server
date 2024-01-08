1037104a27c7abb5e6af3dda710fe887e55bf1dc
manuelguijarro <manu2506.1994@gmail.com>
2024-01-09 00:57:34 +0100

    add cache_fastapi

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from time import time
 from fastapi import FastAPI,__version__
 from routers.users_db import router as user_router
 from fastapi.middleware.cors import CORSMiddleware
-
+from cache_fastapi.cacheMiddleware import CacheMiddleware
 app = FastAPI() # This is what will be refrenced in config
 origins = ["https://backend-lotr-api-vercel-server.vercel.app/"]
 app.include_router(user_router)
+cached_endpoints = [
+    "/routers/"
+]
+"""
 # Añade el middleware CORS
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +19,8 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Content-Type", "Authorization"],
 )
+"""
+app.add_middleware(CacheMiddleware, cached_endpoints=cached_endpoints)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
